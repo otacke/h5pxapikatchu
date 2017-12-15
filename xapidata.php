0dfd351f2260d5e431138ed5a38d7da2d8364787
Oliver Tacke <o.tacke@posteo.de>
2017-12-15 10:07:38 +0100

    add frame for xapidata

--- xapidata.php
+++ xapidata.php
@@ -10,7 +10,7 @@ namespace H5PXAPIKATCHU;
  */
 class XAPIDATA {
 
-  private $json;
+  private $raw;
   private $data;
 
   /**
@@ -19,10 +19,34 @@ class XAPIDATA {
   public function __construct( $xapi ) {
     // Change from JavaScript
     $xapi = str_replace('\"', '"', $xapi);
-    $this->json = $xapi;
+    $this->raw = $xapi;
 
   	$this->data = json_decode($xapi, true);
   }
 
+  /**
+   * Get raw xAPI data.
+   */
+  public function get_raw_xapi() {
+    return $this->$raw
+  }
+
   // TODO: get functions that form the output as needed
+  public function get_actor() {
+
+  }
+
+  public function get_verb() {
+
+  }
+
+  public function get_object() {
+
+  }
+
+  public function get_result() {
+
+  }
+
+  // TODO: generic funtions for forming, e. g. array to string
 }
