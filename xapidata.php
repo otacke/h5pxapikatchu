5f750fecfce8dfffa809dbc2835f3a75a608e4fe
Oliver Tacke <o.tacke@posteo.de>
2017-12-16 15:20:09 +0100

    complete get_actor()

--- xapidata.php
+++ xapidata.php
@@ -4,6 +4,10 @@ namespace H5PXAPIKATCHU;
 
 /**
  * Pseudo xAPI
+ * The goal of this class is not to comply to the xAPI specification, but to
+ * create output that's useful for the user.
+ *
+ * https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md
  *
  * @package H5PXAPIKATCHU
  * @since 0.1
@@ -22,6 +26,8 @@ class XAPIDATA {
     $this->raw = $xapi;
 
   	$this->data = json_decode($xapi, true);
+
+    error_log( print_r( $this->get_actor(), true ) );
   }
 
   /**
@@ -31,22 +37,135 @@ class XAPIDATA {
     return $this->$raw;
   }
 
-  // TODO: get functions that form the output as needed
+  /**
+   * Get flattened actor data from xAPI statement.
+   */
   public function get_actor() {
+    if ( ! array_key_exists( 'actor', $this->data ) ) {
+      return '';
+    }
+    $actor = $this->data['actor'];
 
+    $object_type = ( array_key_exists( 'objectType', $actor ) ) ? $actor['objectType'] : '';
+    $inverse_functional_identifier = $this->flatten_inverse_functional_identifier ( $actor );
+    $name = ( array_key_exists( 'name', $actor )) ? $actor['name'] : '';
+    $members = ( array_key_exists( 'member', $actor) ) ? $this->flatten_members( $actor['member'] ) : '';
+
+    // Identified Group or Anonymous Group (we don't need to distinguish here)
+    if ( $object_type === 'Group' ) {
+      $name = ($name !== '') ? $name . ' (' . __( 'Group' , 'H5PXAPIKATCHU' ) . ')' : $name;
+    }
+
+    //Agent
+    if ( $object_type === 'Agent' || $object_type === '') {
+      // Not really neccessary, but according to xAPI specs agents have no member data
+      $members = '';
+    }
+
+    return [
+      'inverse_functional_identifier' => $inverse_functional_identifier,
+      'name' => $name,
+      'members' => $members
+    ];
   }
 
   public function get_verb() {
-
+    // id = DBID
+    // [display] => extract language code
+    // USE display (id)
   }
 
   public function get_object() {
-
+    // id = DBID
+    // [activity] = USE [name][: description] extract language code (id)
+    // [choices] => should be evaluated
+    // [correctResponsesPattern] => should be evaluated ...
+    //
   }
 
   public function get_result() {
-
+    // id = AUTO_INCREMENT
+    // [response]
+    // [score raw]
+    // [score scaled]
+    // USE score_raw [(score scaled %)]
+    // [completed]
+    // [success]
+    // [duration]
   }
 
-  // TODO: generic funtions for forming, e. g. array to string
+  /**
+   * Flatten xAPI member object.
+   */
+  private function flatten_members ( $members ) {
+    if ( ! is_array( $members ) || empty( $members ) ) {
+      return '';
+    }
+
+    $output = array();
+    foreach ($members as $member) {
+      array_push( $output, $this->flatten_agent( $member ) );
+    }
+    return implode( $output, ', ' );
+  }
+
+  /**
+   * Flatten xAPI agent object.
+   */
+  private function flatten_agent ( $agent ) {
+    if ( ! is_array( $agent ) || empty( $agent ) ) {
+      return '';
+    }
+
+    $name = ( array_key_exists( 'name', $agent ) ) ? $agent['name'] : '';
+    $ifi = $this->flatten_inverse_functional_identifier( $agent );
+
+    if ( $name !== '' && $ifi !== '' ) {
+      $name = ' (' . $name . ')';
+    }
+
+    return $ifi . $name ;
+  }
+
+  /**
+   * Flatten xAPI InverseFunctionalIdentifier object.
+   */
+  private function flatten_inverse_functional_identifier ( $actor ) {
+    if ( ! is_array( $actor ) || empty( $actor ) ) {
+      return '';
+    }
+
+    $inverse_functional_identifier = array();
+    if ( array_key_exists( 'mbox', $actor ) ) {
+      array_push( $inverse_functional_identifier, __( 'email', 'H5PXAPIKATCHU' ) . ': ' . $actor['mbox'] );
+    }
+    if ( array_key_exists( 'mbox_sha1sum', $actor ) ) {
+      array_push( $inverse_functional_identifier, __( 'email hash', 'H5PXAPIKATCHU' ) . ': ' . $actor['mbox_sha1sum'] );
+    }
+    if ( array_key_exists( 'openid', $actor ) ) {
+      array_push( $inverse_functional_identifier, __( 'openid', 'H5PXAPIKATCHU' ) . ': ' . $actor['openid'] );
+    }
+    if ( array_key_exists( 'account', $actor ) ) {
+      array_push( $inverse_functional_identifier, __( 'account', 'H5PXAPIKATCHU' ) . ': ' . $this->flatten_account( $actor['account'] ) );
+    }
+    return ( empty( $inverse_functional_identifier ) ) ? '' : implode( $inverse_functional_identifier, ', ' );
+  }
+
+  /**
+   * Flatten xAPI account object.
+   */
+  private function flatten_account ( $account ) {
+    if ( ! is_array( $account ) || empty( $account ) ) {
+      return '';
+    }
+
+    $name = ( array_key_exists( 'name', $account ) ) ? $account['name'] : '';
+    $homepage = ( array_key_exists( 'homePage', $account ) ) ? $account['homePage'] : '';
+
+    if ( $name !== '' && $homepage !== '' ) {
+      $homepage = ' (' . $homepage . ')';
+    }
+
+    return $name . $homepage;
+  }
 }
